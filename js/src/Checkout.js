ba8040a1b6b67af19f459c28852076509bfec483
Vicente Baixauli <vibaiher@gmail.com>
2019-03-28 15:09:27 +0100

    Solución en JS, fina, fina

--- js/src/Checkout.js
+++ js/src/Checkout.js
@@ -1,11 +1,24 @@
 class Checkout {
-    constructor(pricing_rules) {
+    constructor(rules) {
+        this.items = []
+        this.rules = rules
+        this.result = 0
+        this.items2 = []
     }
 
     scan(item) {
+        this.items.push(item)
     }
 
     total() {
+        this.items.forEach(e => {
+            this.items2.push(e)
+            this.result += this.rules.table()[e]
+            let i = 0
+            this.items2.forEach(ee => { if (ee == e) { i += 1 } })
+            e == 'A' && i % 3 == 0 ? this.result -= 20 : e == 'B' && i % 2 == 0 ? this.result -= 15 : this.result
+        })
+        return this.result
     }
 }
 
--- js/src/PricingRules.js
+++ js/src/PricingRules.js
@@ -1,4 +1,12 @@
 class PricingRules {
+    table() {
+        return {
+            'A': 50,
+            'B': 30,
+            'C': 20,
+            'D': 15
+        }
+    }
 }
 
 module.exports = PricingRules
