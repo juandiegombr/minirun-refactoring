60a92f87626b60ef5a38a0afbd7253e617bc86d8
Vicente Baixauli <vibaiher@gmail.com>
2019-03-28 15:04:33 +0100

    Solución en Ruby, fina, fina

--- ruby/lib/checkout.rb
+++ ruby/lib/checkout.rb
@@ -1,10 +1,23 @@
 class Checkout
-  def initialize(pricing_rules)
+  def initialize(rules)
+    @items = []
+    @rules = rules
+    @result = 0
+    @items2 = []
   end
 
   def scan(item)
+    @items << item
   end
 
   def total
+    @items.each do |e|
+      @items2 << e
+      @result += @rules.table[e]
+      i = 0
+      @items2.each { |ee| i += 1 if ee == e }
+      e == 'A' && i % 3 == 0 ? @result -= 20 : e == 'B' && i % 2 == 0 ? @result -= 15 : @result
+    end
+    @result
   end
 end
--- ruby/lib/pricing_rules.rb
+++ ruby/lib/pricing_rules.rb
@@ -1,2 +1,10 @@
 class PricingRules
+  def table
+    {
+      'A' => 50,
+      'B' => 30,
+      'C' => 20,
+      'D' => 15
+    }
+  end
 end
