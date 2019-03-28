cc2633a8093967d93b3fa9adceeca5f7fbfe91db
Vicente Baixauli <vibaiher@gmail.com>
2019-03-28 15:07:59 +0100

    Solución en PHP, fina, fina

--- php/src/Checkout.php
+++ php/src/Checkout.php
@@ -5,13 +5,27 @@ namespace Checkout;
 use Checkout\PricingRules;
 
 class Checkout {
+    private $rules;
+
     public function __construct(PricingRules $rules) {
+        $this->items = [];
+        $this->rules = $rules;
+        $this->items2 = [];
+        $this->result = 0;
     }
 
-    public function scan($item) {
+    public function scan(String $item) {
+        $this->items[] = $item;
     }
 
     public function total() {
-        return 0;
+        foreach($this->items as $e) {
+            $this->items2[] = $e;
+            $this->result += $this->rules->table()[$e];
+            $i = 0;
+            foreach($this->items2 as $ee) { if ($ee == $e) $i += 1; }
+            $e == 'A' && $i % 3 == 0 ? $this->result -= 20 : ($e == 'B' && $i % 2 == 0 ? $this->result -= 15 : $this->result);
+        }
+        return $this->result;
     }
 }
--- php/src/PricingRules.php
+++ php/src/PricingRules.php
@@ -3,4 +3,12 @@
 namespace Checkout;
 
 class PricingRules {
+    public function table() {
+        return array(
+            'A' => 50,
+            'B' => 30,
+            'C' => 20,
+            'D' => 15
+        );
+    }
 }
